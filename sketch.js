4e4413696fc93bfd34697f2a21382a6e600c5d94
Gustav Paragas <gep21b@fsu.edu>
2022-11-08 17:35:20 -0500

    game updates

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(4000, 4000);
+  createCanvas(2000, 2000);
   rectMode("CENTER");
   angleMode('DEGREES');
 
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   background(0);
 
-  translate(2000, 2000);
+  translate(1000, 1000);
   rotate(-90);
     let hr = hour();
     let mn = minute();
@@ -23,17 +23,17 @@ function draw() {
     stroke(255, 100, 150);
     noFill();
     let secondAngle = map(sc, 0, 59, 0, 360); 
-    arc (0, 0, 3000, 3000, 0, secondAngle);
+    arc (0, 0, 1800,1800, 0, secondAngle);
 
     stroke(150, 100, 255);
     let minAngle = map(mn, 0, 59, 0, 360);
-    arc(0, 0, 2800, 2800, 0, minAngle);
+    arc(0, 0, 1600, 1600, 0, minAngle);
 
     stroke(150, 255, 100);
     let hourAngle = map(hr % 12, 0, 12, 0, 360); 
-    arc (0, 0, 2600, 2600, 0, hourAngle);
+    arc (0, 0, 1400, 1400, 0, hourAngle);
 }
 
 function mouseClicked() {
-  console.log("go to text.js");
+  console.log("go to text2.js");
 }
